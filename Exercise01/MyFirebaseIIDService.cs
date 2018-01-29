102b35cf5af491454367a2b18540a5d74c419977
U-DESKTOP-9OIDHC8\MSI <huynhngocminh2511@gmail.com>
2018-01-29 18:53:35 +0700

    Add Exercise01

---
+++ Exercise01/MyFirebaseIIDService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Iid;
+using Android.Util;
+
+namespace Exercise01
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class MyFirebaseIIDService : FirebaseInstanceIdService
+    {
+        const string TAG = "MyFirebaseIIDService";
+        public override void OnTokenRefresh()
+        {
+            base.OnTokenRefresh();
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+        }
+    }
+}
---
+++ Exercise01/MyFirebaseMessagingService.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Messaging;
+using Android.Media;
+using Android.Support.V4.App;
+using Java.Util;
+using System;
+using Java.Lang;
+
+namespace Exercise01
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    [IntentFilter(new[] { global::Android.Content.Intent.ActionBootCompleted })]
+    class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "MyFirebaseMsgService";
+
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            base.OnMessageReceived(message);
+            Android.Util.Log.Debug(TAG, "From: " + message.From);
+            Android.Util.Log.Debug(TAG, "Notification Message Body: " + message.GetNotification());
+            SendNotification(message.GetNotification().Title, message.GetNotification().Body);
+        }
+
+        private void SendNotification(string title, string body)
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
+
+            var defaultSoundUri = RingtoneManager.GetDefaultUri(RingtoneType.Notification);
+            var notificationBuilder = new NotificationCompat.Builder(this)
+                .SetSmallIcon(Resource.Drawable.common_google_signin_btn_icon_dark)
+                .SetContentTitle(title)
+                .SetContentText(body)
+                .SetAutoCancel(true)
+                .SetSound(defaultSoundUri)
+                .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManager.FromContext(this);
+            notificationManager.Notify(0, notificationBuilder.Build());
+        }
+    }
+}
